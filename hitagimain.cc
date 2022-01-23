f0cbced37f9d29ca9e2e1e12a1ba1f8d2f9ca537
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-01-23 10:18:39 -0800

    pair down more features

--- hitagimain.cc
+++ hitagimain.cc
@@ -1,6 +1,10 @@
 //
 // Created by jwscoggins on 6/7/21.
 //
+#include <cstdint>
+
+template<typename T>
+inline volatile T& memory(const uint32_t address) { return *reinterpret_cast<T*>(address); }
 
 int main() {
     return 0;
